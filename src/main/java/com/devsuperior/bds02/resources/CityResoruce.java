c71f4639a62aa2cdc2c44e37b1ae9d7d04b76222
lfelipe <rafael.eng.mecanic@gmail.com>
2022-01-15 11:50:16 -0300

    testes passando

---
+++ src/main/java/com/devsuperior/bds02/resources/CityResoruce.java
@@ -0,0 +1,39 @@
+package com.devsuperior.bds02.resources;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityResoruce {
+
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll(){
+    List<CityDTO> list = cityService.findAll();
+    return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert( @RequestBody CityDTO dto) {
+        dto = cityService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) throws Exception {
+        cityService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
